1dcce4797ce9df330ed29066ebcebcb41b25040e
Jasmin <jasmin.guay@mail.concordia.ca>
2022-10-02 20:19:21 -0400

    Adding ci configs + first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dummy server to test the CI CD pipeline.
+
+const express = require("express");
+const cors = require("cors");
+
+
+
+const app = express();
+
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
+
+module.exports = app;
