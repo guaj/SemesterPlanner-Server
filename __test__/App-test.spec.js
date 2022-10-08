90266a5fe1dbb1994a77f6159287b9fc028d86a4
Jasmin <jasmin.guay@mail.concordia.ca>
2022-10-08 15:54:38 -0400

    Added exemple of running test

---
+++ __test__/App-test.spec.js
@@ -0,0 +1,7 @@
+
+const sum = require("../src/sum");
+
+test("It shoud sum to numbers", () => {
+  expect(sum(2,3)).toBe(5);
+
+});
--- src/server.js
+++ src/server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  PORT,
-  console.log(
+PORT,
+console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+)
 );
 
 app.get("/", (req, res) => res.send("API Running"));
