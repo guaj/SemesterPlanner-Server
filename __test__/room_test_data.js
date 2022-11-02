809df0cf032906a38b8d7878d0ba8b0422cce002
RamB98 <ramzi2009@live.ca>
2022-11-02 19:10:04 -0400

    created test file  for room

---
+++ __test__/room_test_data.js
@@ -0,0 +1,10 @@
+roomdata = {
+    "owner":"ram68gno@gmail.com",
+    "color":"red",
+    "description":"2nd best room",
+    "avatarText":"cr",
+    "title":"cool room"
+  }
+
+
+  module.exports = {roomdata};
