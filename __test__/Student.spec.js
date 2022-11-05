ab067cee3d0e33ff0773f807d53962d2e7191c77
jacobmathewlopez <59061034+jacobmathewlopez@users.noreply.github.com>
2022-11-05 16:19:20 -0400

    Update

--- __test__/Student.spec.js
+++ __test__/Student.spec.js
@@ -9,12 +9,10 @@ const dbHandler = require("./db-handler");
 /**
  * Connect to a new in-memory database before running any tests.
  */
-beforeAll(async () => await dbHandler.connect());
-
-/**
- * Clear all test data after every test.
- */
-afterEach(async () => await dbHandler.clearDatabase());
+beforeAll(async () => {
+  await dbHandler.connect()
+  await dbHandler.clearDatabase()
+});
 
 /**
  * Remove and close the db and server.
