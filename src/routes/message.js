0f08bc4b2ba0b5e88169d97caa475f39c57488ac
PhunnieB <phunniehypnotix@gmail.com>
2022-11-07 23:30:56 -0500

    get message by ID, get messages in bulk and message culling
    Added routes to:
    get messages by their message ID
    get messages in bulk from room
    
    Added automatic culling of messages directly embedded in a study room. Retrieve extra messages from the added routes.

--- src/routes/message.js
+++ src/routes/message.js
@@ -5,13 +5,17 @@ const Message = require('../models/message.model');
 const CreateMesage = require("../factory/MessageFactory");
 const CreateMessage = require("../factory/MessageFactory");
 
-//Sending a message, requires sID (study room ID it belongs to), username and content
+// Sending a message, requires sID (study room ID it belongs to), username and content
+// By default, this will limit the room messages to 30 to reduce load and document size. Use the /bulk routes below to retrieve more messages.
 router.route('/send').post(async (req, res) => {
     const sID = req.body.sID;
     const msg = CreateMessage(req.body);
     const room = await StudyRoom.findOne({sID: sID})
     const messages = room.messages;
 
+    while (messages.length >= 30) {
+      messages.pop();
+    }
     messages.unshift(msg)
 
     await StudyRoom.updateOne({sID: sID}, {messages: messages})
@@ -20,5 +24,33 @@ router.route('/send').post(async (req, res) => {
     var io = req.app.get('socketio');
     io.to(sID).emit('newMessage', msg)
   });
+
+// finding message by message ID
+router.route('/:mID').get(async (req, res) => {
+  Message.findOne({email: req.params.email})
+  .then(msg => res.json(msg).status(200))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// get 'amount' most recent messages in studyroom with ID sID
+router.route('/bulk/:sID/:amount').get(async (req, res) => {
+  const limit = parseInt(req.params.amount)
+   Message.find({sID : req.params.sID}).sort({_id:-1}).limit(limit)
+  .then(messages => res.json(messages).status(200))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// get 'amount' - 'ignore' most recent messages in studyroom with ID sID
+// the query to the db does not change, the response returned will be smaller depending on the amount of messages to ignore
+// ex: /bulk/:sID/15/5 would return an array of the 10 most recent messages after the first 5.
+router.route('/bulk/:sID/:amount/:ignore').get(async (req, res) => {
+  const limit = parseInt(req.params.amount)
+  Message.find({sID : req.params.sID}).sort({_id:-1}).limit(limit)
+  .then(messages => {
+    messages.splice(0, parseInt(req.params.ignore))
+    res.json(messages).status(200)
+  })
+  .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
