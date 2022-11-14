9ee33a77acdf88cd8ec6aeb8d4debfcb34c8b913
PhunnieB <phunniehypnotix@gmail.com>
2022-11-14 04:44:01 -0500

    formatting

--- __test__/room.spec.js
+++ __test__/room.spec.js
@@ -1,16 +1,7 @@
 // INCOMPLETE
 const { request, assert } = require("./helper/app");
 const { roomdata } = require("./helper/room_test_data");
-
 const dbHandler = require('./helper/db-handler')
-
-//These tests will work locally, but not in CicleCI because we do not have a public API available.
-//Change the URL when that is setup.
-jest.useRealTimers();
-const random0 = (Math.random() + 8).toString(36).substring(2);
-const random2 = (Math.random() + 7).toString(36).substring(2);
-const random3 = (Math.random() + 6).toString(36).substring(2);
-const random4 = (Math.random() + 4).toString(36).substring(2);
 
 /**
  * Connect to a new in-memory database before running any tests.
