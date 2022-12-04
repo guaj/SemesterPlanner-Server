4367ddffe1347ca65e172710a1c6b48b45795182
jacobmathewlopez <59061034+jacobmathewlopez@users.noreply.github.com>
2022-12-04 15:20:32 -0500

    Merge

--- src/routes/openData.js
+++ src/routes/openData.js
@@ -1,40 +1,137 @@
+const OpenDataFacultyRepository = require("../repository/conUOpenDataFacultyRepository");
+const OpenDataCourseRepository = require("../repository/conUOpenDataCourseRepository");
+const OpenDataImportantDateRepository = require("../repository/conUOpenDataImportantDateRepository");
 const router = require('express').Router();
-const axios = require('axios');
+
+
+/** runs once on server start then refreshes open data once every 24 hours (if --odrefreshinterval param is not
+ * specified or is more that 24 [days]; else refreshes every 'odrefreshinterval' days); open data refresh is not
+ * run if --odrefresh param is not set to true
+ */
+if (process.env.npm_config_odrefresh === "true")
+    setInterval(
+        function openDataRefresh() {
+            OpenDataFacultyRepository.refreshFacultyData();
+            OpenDataCourseRepository.refreshCourseData();
+            OpenDataImportantDateRepository.refreshImportantDateData();
+
+            return openDataRefresh;
+        }(), (process.env.npm_config_odrefreshinterval && Number.isInteger(Number(process.env.npm_config_odrefreshinterval) && Number(process.env.npm_config_odrefreshinterval) <= 24) ? (Number(process.env.npm_config_odrefreshinterval) * 86400000) : 86400000));
 
 /**
- * Get specific schedule
+ * add a faculty to a department
+ * @param {String} facultyCode: passed in the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} facultyDescription: passed in the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} departmentCode: passed in the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} departmentDescription: passed in the request body and added to the newly created record in the opendatafaculties collection
+ * @returns {string} Returns a string with the operation's result.
  */
-router.route('/schedule/:courseId/:subject/:catalog').get(async (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'https://opendata.concordia.ca/API/*');
-    try {
-        const scheduleData = await fetchConcordiaSchedule(req.params.courseId, req.params.subject, req.params.catalog)
-            console.log(`scheduleData`, scheduleData)
-            return scheduleData
-    } catch (err) {
-        res.status(400).json('Error: ' + err)
-    }
-    ;
+router.route('/faculty/').post((req, res) => {
+    OpenDataFacultyRepository.create(req.body)
+        .then((faculty) => {
+            res.json(`Faculty record ${faculty._id} created.`).status(200);
+            console.log(`Faculty record ${faculty._id} created:\n` + faculty + "\n");
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 /**
- * Service to fetch current Concordia schedule for the given parameters
- * @param courseId ID of unique course (ex. Course 002625 of BIOL201)
- * @param subject Subject of course (ex. BIOL)
- * @param catalog Course number (ex. 201)
- * @returns {Promise<void>}
+ * get list of all faculty codes and faculty descriptions in the university
+ * @returns [faculties] Returns an array of faculty codes and faculty descriptions in the university.
  */
-const fetchConcordiaSchedule = async (courseId, subject, catalog) => {
-    await axios.get(`https://opendata.concordia.ca/API/v1/course/schedule/filter/${courseId}/${subject}/${catalog}`, {
-        auth: {
-            username: process.env.OPEN_DATA_USERNAME,
-            password: process.env.OPEN_DATA_PASSWORD
-        }
-    }).then((scheduleData) => {
-        console.log(`scheduleData.data`,scheduleData.data)
-        return scheduleData.data
-    }).catch((e) => {
-        console.log(`Error: ${e}`)
+router.route('/faculty/').get(async (req, res) => {
+    OpenDataFacultyRepository.getFacultyList()
+        .then((faculty) => {
+            res.json(faculty).status(200);
+            console.info("List of faculties fetched:");
+            console.info(faculty);
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+/**
+ * find all departments in a faculty
+ * @param {String} facultyCode: passed in URL corresponds to the faculty of interest
+ * @returns {[Faculty]} Returns an array of Faculty with the provided facultyCode param.
+ */
+router.route('/faculty/:facultyCode').get(async (req, res) => {
+    const facultyCode = req.params.facultyCode.toString()
+    OpenDataFacultyRepository.findAllByFacultyCode(facultyCode)
+        .then((faculty) => {
+            res.json(faculty).status(200);
+            console.info(`List of departments in faculty \'${facultyCode}\' fetched:\n` + faculty + "\n")
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+/**
+ * add a course
+ * @param {String} ID, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} title, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} subject, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} catalog, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} career, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} classUnit, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} prerequisites, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @param {String} crosslisted, passed as part of the request body and added to the newly created record in the opendatafaculties collection
+ * @returns {String} Returns a string with the operation's result.
+ */
+router.route('/course/').post((req, res) => {
+    OpenDataCourseRepository.create(req.body)
+        .then((course) => {
+            res.json(`Course record ${course._id} created.`).status(200);
+            console.log(`Faculty record ${faculty._id} created:\n` + faculty + "\n");
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+/**
+ * find all courses by course code
+ * @param {String} courseCode: passed in URL corresponds to the course code (eg. ENCS, SOEN, ENGR) of interest
+ * @returns {[Course]} Returns an array of Courses associated with the courseCode param.
+ */
+router.route('/course/:courseCode').get(async (req, res) => {
+    const courseCode = req.params.courseCode.toString();
+    OpenDataCourseRepository.findAllByCourseCode(courseCode)
+        .then((course) => {
+            res.json(course).status(200);
+            console.info(`List of courses with course code \'${courseCode}\' fetched:\n` + course + "\n")
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+/**
+ * find a specific course by course code and course number
+ * @param {String} courseCode: passed in URL correspond to the course code (eg. ENCS, SOEN, ENGR)
+ * @param {String, Integer} courseNumber: passed in URL correspond to the course number (eg. 282, 490, 301)
+ * @returns {[Course]} Returns an array containing a Course record associated with the courseCode and courseNumber param.
+ */
+router.route('/course/:courseCode/:courseNumber').get(async (req, res) => {
+    const courseCode = req.params.courseCode.toString();
+    const courseNumber = req.params.courseNumber.toString();
+    OpenDataCourseRepository.findByCourseCodeAndNumber(courseCode, courseNumber)
+        .then((course) => {
+            res.json(course).status(200);
+            console.info(`Record of course with course \'${courseCode} ${courseNumber}\' fetched:\n` + course + "\n")
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+/**
+ * get all important dates
+ * @returns {[ImportantDate]} Returns an array with the ImportantDate records.
+ */
+router.route('/importantdates/').get(async (req, res) => {
+    console.info(`Important dates requested.`)
+
+    OpenDataImportantDateRepository.getAllImportantDates().then((importantDates) => {
+        res.json(importantDates).status(200);
+        console.info(`Important dates returned.`);
+    }).catch((err) => {
+        console.error(err);
+        res.status(400).json('Error: ' + err);
     })
-}
+})
 
 module.exports = router;
+
