7769f66e68b5e7f6d97b0ef36071e0ca1af0be06
mnash <m.nashatelsayed@gmail.com>
2022-12-15 15:32:44 -0500

    - added endpoint that receives a file directly as form-data and returns the text from the image - updated endpoint path for url image text extraction from 'tesseract/' to 'tesseract/url/'

--- src/routes/tesseract.js
+++ src/routes/tesseract.js
@@ -7,6 +7,12 @@
 const tesseract = require("node-tesseract-ocr")
 const router = require('express').Router();
 
+const Multer = require('multer');
+// create multer instance
+const multer = Multer({
+    storage: Multer.memoryStorage()
+});
+
 const config = {
     lang: "eng",
     oem: 1,
@@ -18,13 +24,34 @@ const config = {
  * @param {String} url, url of image from which text will be extracted
  * @returns {String}, text from the image url provided
  */
-router.route("/").post((req,res) => {
+router.route("/url/").post((req, res) => {
     const imgUrl = req.body.url;
 
     tesseract
         .recognize(imgUrl, config)
         .then((text) => {
             console.log("Result:", text);
+            res.json(text).status(200);
+        })
+        .catch((error) => {
+            console.log(error.message)
+            res.json(error.message).status(400);
+        })
+})
+
+/**
+ * Route that obtains text from an image file
+ * @param {file} img, image file from which text will be extracted; pass the image under the field name 'img'
+ * as **form-data**, not x-www-form-urlencoded
+ * @returns {String}, text from the image url provided
+ */
+router.route("/img/").post(multer.single("img"), (req, res) => {
+    const img = req.file;
+
+    tesseract
+        .recognize(img.buffer, config)
+        .then((text) => {
+            console.log("Result:\n", text);
             res.json(text).status(200);
         })
         .catch((error) => {
